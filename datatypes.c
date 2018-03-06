73db7877a6634c6200d113e80b953ee9c19c3cbe
Monali Patil <monali24u@gmail.com>
2018-03-06 11:17:44 -0700

    accessing different datatypes.

---
+++ datatypes.c
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+// Declare second integer, double, and String variables.
+ int a;
+ double b;
+ string c;
+
+ // Read and save an integer, double, and String to your variables.
+ // Note: If you have trouble reading the entire string, please go back and review the Tutorial closely.
+   cin>>a;
+   cin>>b;
+   getline(cin, c);
+   getline(cin, c);
+ // Print the sum of both integer variables on a new line.
+   a = a + i;
+  cout<<a<<endl;
+
+ // Print the sum of the double variables on a new line.
+  b = b + d;
+  cout<<fixed<<setprecision(1)<<b<<endl;
+
+ // Concatenate and print the String variables on a new line
+ // The 's' variable above should be printed first.
+ cout<<s<<c<<endl;
+
+ return 0;
+}
